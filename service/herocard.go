1a420aa03bedce17b1f368d2c05e2e792a503053
ygo <ka.ra.si.naporitan@gmail.com>
2023-09-29 14:46:25 +0900

    カードゲット

--- service/herocard.go
+++ service/herocard.go
@@ -18,5 +18,38 @@ func NewHeroCardService(db *sql.DB) *HeroCardService {
 }
 
 func (s *HeroCardService) GetHeroCardList() ([]*model.Rider, error) {
-	return nil,nil
+	const sqlStr = `SELECT * FROM rider`
+	rows, err := s.db.Query(sqlStr)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var riders []*model.Rider
+	for rows.Next() {
+		var rider model.Rider
+		if err := rows.Scan(
+			&rider.Number,
+			&rider.Name,
+			&rider.Rarity,
+			&rider.Category,
+			&rider.Attribute,
+			&rider.Attack,
+			&rider.Defence,
+			&rider.HitPoint,
+			&rider.FinishingAttack,
+			&rider.SkillName,
+			&rider.AttackTeki,
+			&rider.DefenceTeki,
+			&rider.HitPointTeki,
+			&rider.FinishingAttackTeki,
+		); err != nil {
+			return nil, err
+		}
+		riders = append(riders, &rider)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return riders, nil
 }
